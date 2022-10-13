f886206f6fe1b6277505496174c9f962d9670d89
Ebi <104730381+Aiuraa@users.noreply.github.com>
2022-10-13 17:03:07 +0700

    Initial commit

---
+++ src/MainForm.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace SkidWare
+{
+    partial class SkidWareForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SkidWareForm));
+            this.richTextBoxScript = new System.Windows.Forms.RichTextBox();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnExecute = new System.Windows.Forms.Button();
+            this.btnInject = new System.Windows.Forms.Button();
+            this.labelLogo = new System.Windows.Forms.Label();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.labelClose = new System.Windows.Forms.Label();
+            this.labelMinimize = new System.Windows.Forms.Label();
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // richTextBoxScript
+            // 
+            this.richTextBoxScript.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBoxScript.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBoxScript.Location = new System.Drawing.Point(58, 147);
+            this.richTextBoxScript.Name = "richTextBoxScript";
+            this.richTextBoxScript.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedBoth;
+            this.richTextBoxScript.Size = new System.Drawing.Size(342, 173);
+            this.richTextBoxScript.TabIndex = 0;
+            this.richTextBoxScript.Text = "Insert your script here";
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(58, 338);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnLoad.TabIndex = 1;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.OnLoadButtonClicked);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(148, 338);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.OnButtonSaveClicked);
+            // 
+            // btnExecute
+            // 
+            this.btnExecute.Location = new System.Drawing.Point(236, 338);
+            this.btnExecute.Name = "btnExecute";
+            this.btnExecute.Size = new System.Drawing.Size(75, 23);
+            this.btnExecute.TabIndex = 3;
+            this.btnExecute.Text = "Execute";
+            this.btnExecute.UseVisualStyleBackColor = true;
+            this.btnExecute.Click += new System.EventHandler(this.OnButtonExecuteClicked);
+            // 
+            // btnInject
+            // 
+            this.btnInject.Location = new System.Drawing.Point(325, 338);
+            this.btnInject.Name = "btnInject";
+            this.btnInject.Size = new System.Drawing.Size(75, 23);
+            this.btnInject.TabIndex = 4;
+            this.btnInject.Text = "Inject";
+            this.btnInject.UseVisualStyleBackColor = true;
+            this.btnInject.Click += new System.EventHandler(this.OnButtonInjectClicked);
+            // 
+            // labelLogo
+            // 
+            this.labelLogo.AutoSize = true;
+            this.labelLogo.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelLogo.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.labelLogo.Location = new System.Drawing.Point(149, 59);
+            this.labelLogo.Name = "labelLogo";
+            this.labelLogo.Size = new System.Drawing.Size(164, 37);
+            this.labelLogo.TabIndex = 5;
+            this.labelLogo.Text = "SkidWare";
+            // 
+            // labelClose
+            // 
+            this.labelClose.AutoSize = true;
+            this.labelClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.labelClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelClose.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.labelClose.Location = new System.Drawing.Point(439, 6);
+            this.labelClose.Name = "labelClose";
+            this.labelClose.Size = new System.Drawing.Size(24, 25);
+            this.labelClose.TabIndex = 6;
+            this.labelClose.Text = "x";
+            this.labelClose.Click += new System.EventHandler(this.OnButtonCloseClicked);
+            // 
+            // labelMinimize
+            // 
+            this.labelMinimize.AutoSize = true;
+            this.labelMinimize.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.labelMinimize.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelMinimize.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.labelMinimize.Location = new System.Drawing.Point(405, 6);
+            this.labelMinimize.Name = "labelMinimize";
+            this.labelMinimize.Size = new System.Drawing.Size(25, 25);
+            this.labelMinimize.TabIndex = 7;
+            this.labelMinimize.Text = "_";
+            this.labelMinimize.Click += new System.EventHandler(this.OnButtonMinimizeClicked);
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.AutoSize = true;
+            this.labelStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelStatus.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.labelStatus.Location = new System.Drawing.Point(55, 128);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(97, 16);
+            this.labelStatus.TabIndex = 8;
+            this.labelStatus.Text = "Status: Loaded";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.linkLabel1.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.linkLabel1.Location = new System.Drawing.Point(8, 407);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(96, 13);
+            this.linkLabel1.TabIndex = 9;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Copyleft 2022 - Ebi";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.OnButtonCopyleftClicked);
+            // 
+            // SkidWareForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MidnightBlue;
+            this.ClientSize = new System.Drawing.Size(468, 431);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.labelStatus);
+            this.Controls.Add(this.labelMinimize);
+            this.Controls.Add(this.labelClose);
+            this.Controls.Add(this.labelLogo);
+            this.Controls.Add(this.btnInject);
+            this.Controls.Add(this.btnExecute);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.richTextBoxScript);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "SkidWareForm";
+            this.Load += new System.EventHandler(this.OnSkidWareFormLoad);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.OnFormMouseDown);
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBoxScript;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnExecute;
+        private System.Windows.Forms.Button btnInject;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.Label labelLogo;
+        private System.Windows.Forms.Label labelClose;
+        private System.Windows.Forms.Label labelMinimize;
+        private System.Windows.Forms.Label labelStatus;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ src/api/Synapse.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using sxlib;
+using sxlib.Specialized;
+using SkidWare;
+
+namespace SkidWare.api
+{
+    class Synapse
+    {
+        // Internal variable, no need to expose
+        private SxLibWinForms instance;
+        private Form mainForm;
+        private Label attachedLabel;
+        private SxLibBase.SynAttachEvents attachEvent;
+
+        public void SynapseLoadEvent(SxLibBase.SynLoadEvents Event, object e)
+        {
+            switch (Event)
+            {
+                case SxLibBase.SynLoadEvents.CHECKING_WL:
+                    attachedLabel.Text = "Status: Checking whitelist";
+                    break;
+                case SxLibBase.SynLoadEvents.ALREADY_EXISTING_WL:
+                    attachedLabel.Text = "Status: Whitelist already existing";
+                    break;
+                case SxLibBase.SynLoadEvents.CHANGING_WL:
+                    attachedLabel.Text = "Status: Changing whitelist";
+                    break;
+                case SxLibBase.SynLoadEvents.CHECKING_DATA:
+                    attachedLabel.Text = "Status: Checking synapse data";
+                    break;
+                case SxLibBase.SynLoadEvents.DOWNLOADING_DATA:
+                    attachedLabel.Text = "Status: Downloading synapse data";
+                    break;
+                case SxLibBase.SynLoadEvents.DOWNLOADING_DLLS:
+                    attachedLabel.Text = "Status: Downloading the DLLs";
+                    break;
+                case SxLibBase.SynLoadEvents.FAILED_TO_DOWNLOAD:
+                    attachedLabel.Text = "Status: Failed to download the data";
+                    break;
+                case SxLibBase.SynLoadEvents.NOT_ENOUGH_TIME:
+                    attachedLabel.Text = "Status: Failed to change whitelist, wait 24 hours to change whitelist";
+                    break;
+                case SxLibBase.SynLoadEvents.NOT_LOGGED_IN:
+                    attachedLabel.Text = "Status: Failed to check whitelist, log-in using official synapse UI";
+                    break;
+                case SxLibBase.SynLoadEvents.NOT_UPDATED:
+                    attachedLabel.Text = "Status: Your synapse file is old, use synapse launcher to update";
+                    break;
+                case SxLibBase.SynLoadEvents.UNAUTHORIZED_HWID:
+                    attachedLabel.Text = "Status: Unauthorized HWID, contact synapse staff if you think this is false error";
+                    break;
+                case SxLibBase.SynLoadEvents.FAILED_TO_VERIFY:
+                    attachedLabel.Text = "Status: Failed to verify files";
+                    MessageBox.Show("SkidWare cannot verify your synapse file, well i guess you get that from unknown source lmao", "SkidWare - Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    this.Unload();
+                    break;
+                case SxLibBase.SynLoadEvents.UNKNOWN:
+                    attachedLabel.Text = "Status: Unknown Error";
+                    MessageBox.Show("SkidWare encountered unknown error, make sure to tell your antivirus to stay the fuck off from synapse", "SkidWare - Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    this.Unload();
+                    break;
+                case SxLibBase.SynLoadEvents.READY:
+                    attachedLabel.Text = "Status: SkidWare is ready to use!";
+                    break;
+            }
+        }
+        public void SynapseAttachEvent(SxLibBase.SynAttachEvents Event, object e)
+        {
+            switch (Event)
+            {
+                case SxLibBase.SynAttachEvents.CHECKING:
+                    attachedLabel.Text = "Status: Checking roblox";
+                    break;
+                case SxLibBase.SynAttachEvents.CHECKING_WHITELIST:
+                    attachedLabel.Text = "Status: Checking whitelist";
+                    break;
+                case SxLibBase.SynAttachEvents.FAILED_TO_ATTACH:
+                    attachedLabel.Text = "Status: Failed to attach";
+                    break;
+                case SxLibBase.SynAttachEvents.FAILED_TO_FIND:
+                    attachedLabel.Text = "Status: Failed to find roblox instances";
+                    break;
+                case SxLibBase.SynAttachEvents.FAILED_TO_UPDATE:
+                    attachedLabel.Text = "Status: Failed to update synapse";
+                    break;
+                case SxLibBase.SynAttachEvents.INJECTING:
+                    attachedLabel.Text = "Status: Injecting";
+                    break;
+                case SxLibBase.SynAttachEvents.NOT_INJECTED:
+                    attachedLabel.Text = "Status: Not injected";
+                    break;
+                case SxLibBase.SynAttachEvents.ALREADY_INJECTED:
+                    attachedLabel.Text = "Status: Already injected bozo";
+                    break;
+                case SxLibBase.SynAttachEvents.NOT_RUNNING_LATEST_VER_UPDATING:
+                    attachedLabel.Text = "Status: SkidWare running on old synapse version";
+                    break;
+                case SxLibBase.SynAttachEvents.NOT_UPDATED:
+                    attachedLabel.Text = "Status: Outdated synapse version";
+                    break;
+                case SxLibBase.SynAttachEvents.PROC_CREATION:
+                    attachedLabel.Text = "Status: Detected roblox new instance";
+                    break;
+                case SxLibBase.SynAttachEvents.PROC_DELETION:
+                    attachedLabel.Text = "Status: Detected roblox instance being deleted";
+                    break;
+                case SxLibBase.SynAttachEvents.REINJECTING:
+                    attachedLabel.Text = "Status: Reinjecting";
+                    break;
+                case SxLibBase.SynAttachEvents.SCANNING:
+                    attachedLabel.Text = "Status: Scanning";
+                    break;
+                case SxLibBase.SynAttachEvents.UPDATING_DLLS:
+                    attachedLabel.Text = "Status: Updating DLLs";
+                    break;
+                case SxLibBase.SynAttachEvents.READY:
+                    attachedLabel.Text = "Status: Successfully attached";
+                    break;
+            }
+
+            attachEvent = Event;
+        }
+
+        public void Initialize(Form form)
+        {
+            // attach instance
+            this.instance = SxLib.InitializeWinForms(form, Directory.GetCurrentDirectory());
+            instance.LoadEvent += this.SynapseLoadEvent;
+            instance.AttachEvent += this.SynapseAttachEvent;
+            
+            instance.Load();
+            mainForm = form;
+        }
+
+        public void BroadcastEventToLabel(Label label)
+        {
+            attachedLabel = label;
+        }
+
+        public bool ExecuteScript(string script)
+        {
+            instance.Execute(script);
+
+            if (attachEvent.Equals(SxLibBase.SynAttachEvents.NOT_INJECTED))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool AttachToRoblox()
+        {
+            return instance.Attach();
+        }
+
+        public void Unload()
+        {
+            instance.Close();
+            mainForm.Close();
+            Application.Exit();
+        }
+    }
+}
